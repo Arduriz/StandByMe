d38db8404223ea319d9b0f25d190981014833924
CHOI Hyunseo <65582244+Arduriz@users.noreply.github.com>
2022-08-10 23:38:17 +0900

    Create StepperMotor_withoutHeader.c

---
+++ StepperMotor_withoutHeader.c
@@ -0,0 +1,44 @@
+int IN1 = 3;
+int IN2 = 4;
+int IN3 = 6;
+int IN4 = 7;
+//int ENA = 9   EnableA를 아두이노와 연결했을 때
+//int ENB = 10  EnableB를 아두이노와 연결했을 때
+ 
+void setup(){
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  //pinMode(ENA,OUTPUT);    
+  //pinMode(ENB,OUTPUT);
+  //digitalWrite(ENA,HIGH);  # EnableA를 점퍼 핀 연결한 환경과 동일하게
+  //digitalWrite(ENB,HIGH);  # EnableB를 점퍼 핀 연결한 환경과 동일하게
+  //analogWrite(ENA, 50);    # 0~255 값으로 속도를 조절하고자 할 때
+  //analogWrite(ENB, 50);    # 0~255 값으로 속도를 조절하고자 할 때
+}
+void loop(){  
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  delay(3);
+    
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  delay(3);
+      
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  delay(3);
+      
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  delay(3); 
+}
