167e52e0b434b9a91d5b092f20ec0b91a0b05d2e
CHOI Hyunseo <65582244+Arduriz@users.noreply.github.com>
2022-08-11 00:18:03 +0900

    Update StepperMotor_withoutHeader.c

--- StepperMotor_withoutHeader.c
+++ StepperMotor_withoutHeader.c
@@ -5,6 +5,8 @@ int IN4 = 7;
 //int ENA = 9;   //EnableA를 아두이노와 연결했을 때, 변수 정의 때문에 활성화가 좋음
 //int ENB = 10;  //EnableB를 아두이노와 연결했을 때, 변수 정의 때문에 활성화가 좋음
 
+int speedDelay = 10; //모터 속도를 위한 딜레이로 낮을 수록 빠름
+
 void setup(){
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
@@ -18,27 +20,55 @@ void setup(){
   //analogWrite(ENB, 50);     //0~255 값으로 속도를 조절하고자 할 때
 }
 void loop(){  
+  //counter clockwise
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,HIGH);
   digitalWrite(IN3,HIGH);
   digitalWrite(IN4,LOW);
-  delay(3);
+  delay(speedDelay);
     
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,HIGH);
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
-  delay(3);
+  delay(speedDelay);
       
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,HIGH);
-  delay(3);
+  delay(speedDelay);
       
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
   digitalWrite(IN3,HIGH);
   digitalWrite(IN4,LOW);
-  delay(3); 
+  delay(speedDelay); 
+  
+  //clockwise
+  /*
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  delay(speedDelay);
+
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  delay(speedDelay);
+
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  delay(speedDelay);
+
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  delay(speedDelay);
+  */
 }
